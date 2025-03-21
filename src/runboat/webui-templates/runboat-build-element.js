e6d173fc4a3c43891af30e79b23b58ab7b495ce3
Aleksandr Kanaev <kanaevad@gmail.com>
2025-03-21 18:44:39 +0100

    fix webui

--- src/runboat/webui-templates/runboat-build-element.js
+++ src/runboat/webui-templates/runboat-build-element.js
@@ -1,108 +1,141 @@
-import {LitElement, html, css} from 'https://unpkg.com/lit@2.1.2?module';
-import 'https://unpkg.com/@github/time-elements@latest?module';
+import { LitElement, html, css } from "https://esm.run/lit@2.1.2?module";
+import "https://esm.run/@github/time-elements@latest?module";
 
 class RunboatBuildElement extends LitElement {
-    static get properties() {
-        return {
-            build: {}
-        }
-    }
+  static get properties() {
+    return {
+      build: {},
+    };
+  }
 
-    constructor() {
-        super();
-        this.build = {};
-    }
+  constructor() {
+    super();
+    this.build = {};
+  }
 
-    undeployed() {
-        this.build = {...this.build, status: null};
-    }
+  undeployed() {
+    this.build = { ...this.build, status: null };
+  }
 
-    static styles = css`
-        .build-card {
-            width: 16.5em;
-            padding: 0.5em;
-            border-radius: 0.5em;
-            background-color: lightgray;
-        }
-        .build-name {
-            font-size: x-small;
-        }
-        .build-status-stopped {
-            background-color: paleturquoise;
-        }
-        .build-status-started {
-            background-color: palegreen;
-        }
-        .build-status-failed {
-            background-color: lightcoral;
-        }
-        time-ago {
-            color: gray;
-            white-space: nowrap;
-        }
-        p {
-            margin-top: 0.5em;
-            margin-bottom: 0.5em;
-        }
+  static styles = css`
+    .build-card {
+      width: 16.5em;
+      padding: 0.5em;
+      border-radius: 0.5em;
+      background-color: lightgray;
+    }
+    .build-name {
+      font-size: x-small;
+    }
+    .build-status-stopped {
+      background-color: paleturquoise;
+    }
+    .build-status-started {
+      background-color: palegreen;
+    }
+    .build-status-failed {
+      background-color: lightcoral;
+    }
+    time-ago {
+      color: gray;
+      white-space: nowrap;
+    }
+    p {
+      margin-top: 0.5em;
+      margin-bottom: 0.5em;
+    }
+  `;
+
+  render() {
+    if (!this.build.name) {
+      return html`<div class="build-card"><p>Build not found...</p></div>`;
+    }
+    return html`
+      <div class="build-card build-status-${this.build.status}">
+        <p class="build-name">${this.build.name}</p>
+        <p>
+          <a href="${this.build.repo_target_branch_link}"
+            >${this.build.commit_info?.repo}
+            ${this.build.commit_info?.target_branch}</a
+          >
+          ${this.build.commit_info?.pr
+            ? html`PR
+                <a href="${this.build.repo_pr_link}"
+                  >${this.build.commit_info?.pr}</a
+                >`
+            : ""}
+          ${this.build.commit_info?.git_commit
+            ? html`(<a href="${this.build.repo_commit_link}"
+                  >${this.build.commit_info?.git_commit.substring(0, 8)}</a
+                >)`
+            : ""}
+          <time-ago datetime="${this.build.created}"></time-ago>
+        </p>
+        <p>
+          ${this.build.status || "undeployed"}
+          ${this.build.status
+            ? html`⦙ 🗒
+                <a href="/api/v1/builds/${this.build.name}/init-log"
+                  >init log</a
+                >`
+            : ""}
+          ${this.build.status == "started"
+            ? html`⦙ 🗒 <a href="/api/v1/builds/${this.build.name}/log">log</a>`
+            : ""}
+          ${this.build.status == "started"
+            ? html`⦙ 🚪
+                <a href="${this.build.deploy_link}" title="Odoo">live</a>
+                <a href="${this.build.deploy_link_mailhog}" title="Mailhog"
+                  >✉</a
+                >`
+            : ""}
+        </p>
+        <p>
+          <button
+            @click="${this.startHandler}"
+            ?disabled="${this.build.status != "stopped" ||
+            this.build.status == RunboatBuildElement.clickedStatus}"
+          >
+            start
+          </button>
+          <button
+            @click="${this.stopHandler}"
+            ?disabled="${this.build.status != "started" ||
+            this.build.status == RunboatBuildElement.clickedStatus}"
+          >
+            stop
+          </button>
+          <button
+            @click="${this.resetHandler}"
+            ?disabled="${this.build.status ==
+            RunboatBuildElement.clickedStatus}"
+          >
+            reset
+          </button>
+        </p>
+      </div>
     `;
+  }
 
-    render() {
-        if (!this.build.name) {
-            return html`<div class="build-card"><p>Build not found...</p></div>`;
-        }
-        return html`
-        <div class="build-card build-status-${this.build.status}">
-            <p class="build-name">${this.build.name}</p>
-            <p>
-                <a href="${this.build.repo_target_branch_link}">${this.build.commit_info?.repo} ${this.build.commit_info?.target_branch}</a>
-                ${this.build.commit_info?.pr?
-                    html`PR <a href="${this.build.repo_pr_link}">${this.build.commit_info?.pr}</a>`:""
-                }
-                ${this.build.commit_info?.git_commit?
-                    html`(<a href="${this.build.repo_commit_link}">${this.build.commit_info?.git_commit.substring(0, 8)}</a>)`:""
-                }
-                <time-ago datetime="${this.build.created}"></time-ago>
-            </p>
-            <p>
-                ${this.build.status || "undeployed"}
-                ${this.build.status?
-                    html`⦙ 🗒 <a href="/api/v1/builds/${this.build.name}/init-log">init log</a>`:""
-                }
-                ${this.build.status == "started"?
-                    html`⦙ 🗒 <a href="/api/v1/builds/${this.build.name}/log">log</a>`:""
-                }
-                ${this.build.status == "started"?
-                   html`⦙ 🚪 <a href="${this.build.deploy_link}" title="Odoo">live</a> <a href="${this.build.deploy_link_mailhog}" title="Mailhog">✉</a>`:""
-                }
-            </p>
-            <p>
-                <button @click="${this.startHandler}" ?disabled="${this.build.status != "stopped" || this.build.status == RunboatBuildElement.clickedStatus}">start</button>
-                <button @click="${this.stopHandler}" ?disabled="${this.build.status != "started" || this.build.status == RunboatBuildElement.clickedStatus}">stop</button>
-                <button @click="${this.resetHandler}" ?disabled="${this.build.status == RunboatBuildElement.clickedStatus}">reset</button>
-            </p>
-        </div>
-        `;
-    }
+  startHandler(e) {
+    this.actionHandler("start");
+  }
 
-    startHandler(e) {
-        this.actionHandler("start");
-    }
+  stopHandler(e) {
+    this.actionHandler("stop");
+  }
 
-    stopHandler(e) {
-        this.actionHandler("stop");
-    }
+  resetHandler(e) {
+    this.actionHandler("reset");
+  }
 
-    resetHandler(e) {
-        this.actionHandler("reset");
-    }
+  static clickedStatus = "⏳";
 
-    static clickedStatus = "⏳";
-
-    actionHandler(action) {
-        this.build.status = RunboatBuildElement.clickedStatus;
-        this.requestUpdate();
-        fetch(`/api/v1/builds/${this.build.name}/${action}`, {method: 'POST'});
-    }
+  actionHandler(action) {
+    this.build.status = RunboatBuildElement.clickedStatus;
+    this.requestUpdate();
+    fetch(`/api/v1/builds/${this.build.name}/${action}`, { method: "POST" });
+  }
 }
 
-export {RunboatBuildElement};
+export { RunboatBuildElement };
